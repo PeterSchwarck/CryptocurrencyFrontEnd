9a0248734a7d6df7d5f9db1aa68a09db42fe5b58
carlos gomez <gomezc958@hotmail.com>
2018-05-30 18:58:14 +0000

    may 24

--- src/js/views/Contactus.jsx
+++ src/js/views/Contactus.jsx
@@ -44,14 +44,14 @@ export class ContactUs extends React.Component {
                         <div className="row">
                             <div className="col-md-6">
                                 <div className="form-group">
-                                    <label htmlFor="form_name">Firstname *</label>
+                                    <label htmlFor="form_name">Firstname</label>
                                     <input id="form_name" type="text" name="name" className="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required."></input>
                                     <div className="help-block with-errors"></div>
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-group">
-                                    <label htmlFor="form_lastname">Lastname *</label>
+                                    <label htmlFor="form_lastname">Lastname</label>
                                     <input id="form_lastname" type="text" name="surname" className="form-control" placeholder="Please enter your lastname *" required="required" data-error="Lastname is required."></input>
                                     <div className="help-block with-errors"></div>
                                 </div>
@@ -60,7 +60,7 @@ export class ContactUs extends React.Component {
                         <div className="row">
                             <div className="col-md-6">
                                 <div className="form-group">
-                                    <label htmlFor="form_email">Email *</label>
+                                    <label htmlFor="form_email">Email</label>
                                     <input id="form_email" type="email" name="email" className="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required."></input>
                                     <div className="help-block with-errors"></div>
                                 </div>
@@ -76,7 +76,7 @@ export class ContactUs extends React.Component {
                         <div className="row">
                             <div className="col-md-12">
                                 <div className="form-group">
-                                    <label htmlFor="form_message">Message *</label>
+                                    <label htmlFor="form_message">Message</label>
                                     <textarea id="form_message" name="message" className="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
                                     <div className="help-block with-errors"></div>
                                 </div>

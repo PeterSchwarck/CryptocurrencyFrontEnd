2a5e3a93534855a0d806d84f66c081fe65e99cfa
carlos gomez <gomezc958@hotmail.com>
2018-05-30 18:43:13 +0000

    may 24

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -7,6 +7,7 @@ import {Scanner} from './views/Introduction.jsx';
 import {Confirm} from './views/Confirm.jsx';
 import {NotificationCard} from './views/NotificationCard.jsx';
 import {HomeEdit} from './views/HomeEdit.jsx';
+import {ContactUs} from './views/Contactus.jsx';
 
 export class Layout extends Flux.View {
     render() {
@@ -22,6 +23,7 @@ export class Layout extends Flux.View {
                             <Route exact path="/confirm" component={Confirm} />
                             <Route exact path="/card" component={NotificationCard} />
                             <Route exact path="/home2" component={HomeEdit} />
+                            <Route exact path="/Contactus" component={ContactUs} />
                             <Route render={() => <h1>Not found!</h1>} />
                         </Switch>
                     </div>

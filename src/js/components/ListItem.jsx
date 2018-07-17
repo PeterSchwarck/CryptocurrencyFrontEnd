b7b706b9490ea44f679c2a013efc61a31f75fe3d
Peter Schwarck <peterschwarck@gmail.com>
2018-07-17 01:13:57 +0000

    another one

--- src/js/components/ListItem.jsx
+++ src/js/components/ListItem.jsx
@@ -6,22 +6,20 @@ export class ListItem extends React.Component{
 
     render(){
         return <div className="notificationListItem">
-                <table>
-                    <tr>
-                        <td>Coin 1</td>
-                        <td>
-                            <span className="badge badge-success">+10%</span>
-                        </td>
-                        <td>
-                            <span className="badge badge-danger">-25%</span>
-                        </td>
-                        <td>
-                            <button onClick={() => this.props.onClose()} type="button" className="btn btn-light trash trashy">
-                                <i className="far fa-trash-alt"></i>
-                            </button>
-                        </td>
-                    </tr>
-                </table>
+                <tr>
+                    <td>Coin 1</td>
+                    <td>
+                        <span className="badge badge-success">+10%</span>
+                    </td>
+                    <td>
+                        <span className="badge badge-danger">-25%</span>
+                    </td>
+                    <td>
+                        <button onClick={() => this.props.onClose()} type="button" className="btn btn-light trash trashy">
+                            <i className="far fa-trash-alt"></i>
+                        </button>
+                    </td>
+                </tr>
             </div>;
     }
 }
